7427c21d4e68661eed3f2da88a0cd15fe88cf483
gerhus <gebisurgi@gmail.com>
2018-02-16 00:28:46 +0100

    test

--- noname/inputs/fingers.js
+++ noname/inputs/fingers.js
@@ -37,12 +37,12 @@ var Fingers = function (_game) {
 
 	_game.render.canvas.addEventListener('touchmove', function (event) {
 		event.preventDefault();
-		for (var i = 0; i < event.changedTouches.length; i++) {
-			var finger = self.getByDistance(event.changedTouches[i]);
-			finger.currentX = event.changedTouches[i].clientX - _game.render.screen.offsetLeft;
-			finger.currentY = event.changedTouches[i].clientY - _game.render.screen.offsetTop;
-			finger.offsetX = event.changedTouches[i].clientX - _game.render.screen.offsetLeft - finger.startX;
-			finger.offsetY = event.changedTouches[i].clientY - _game.render.screen.offsetTop - finger.startY;
+		for (var i = 0; i < event.touches.length; i++) {
+			var finger = self.getByDistance(event.touches[i]);
+			finger.currentX = event.touches[i].clientX - _game.render.screen.offsetLeft;
+			finger.currentY = event.touches[i].clientY - _game.render.screen.offsetTop;
+			finger.offsetX = event.touches[i].clientX - _game.render.screen.offsetLeft - finger.startX;
+			finger.offsetY = event.touches[i].clientY - _game.render.screen.offsetTop - finger.startY;
 		}
 	}, false);
 
