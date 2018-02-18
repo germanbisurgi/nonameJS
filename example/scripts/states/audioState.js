bf3d7bf610104f54faedd7ca6374b17b57a1fc26
gerhus <gebisurgi@gmail.com>
2018-02-18 12:19:51 +0100

    test

--- example/scripts/states/audioState.js
+++ example/scripts/states/audioState.js
@@ -3,6 +3,7 @@ var audioState = new noname.state('audioState');
 audioState.preload = function () {
 
 	audioState.assets.queueAudio('kick', 'example/assets/audio/kick.wav');
+	audioState.assets.queueAudio('tic', 'example/assets/audio/tic.mp3');
 
 };
 
@@ -10,6 +11,11 @@ audioState.create = function () {
 
 	audioState.kick = audioState.audio.createTrack({
 		audioBuffer: audioState.assets.get('kick'),
+		volume: 1.0
+	});
+
+	audioState.tic = audioState.audio.createTrack({
+		audioBuffer: audioState.assets.get('tic'),
 		volume: 1.0
 	});
 
@@ -21,6 +27,10 @@ audioState.update = function () {
 
 
 	audioState.fingers.justTouched(1, function (_finger) {
+		audioState.tic.play();
+	});
+
+	audioState.fingers.justTouched(2, function (_finger) {
 		audioState.kick.play();
 	});
 
