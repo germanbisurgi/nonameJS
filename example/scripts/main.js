ddde2b2e296640acb172617d86b8dae3d1eb991e
gerhus <gebisurgi@gmail.com>
2018-04-05 20:50:18 +0200

    reset

--- example/scripts/main.js
+++ example/scripts/main.js
@@ -1,7 +1,7 @@
 var game = new noname.game({
 	screen: document.querySelector('.screen'),
-	fps: 30,
-	dps: 30,
+	fps: 60,
+	dps: 60,
 	states: [
 		// timeState,
 		pointersState,
@@ -11,7 +11,7 @@ var game = new noname.game({
 		entitiesState
 	],
 	initialState: 'entitiesState',
-	physicsDebug: false
+	physicsDebug: true
 });
 
 var loading = document.querySelector('.loading');
