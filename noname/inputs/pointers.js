4f2898051cdd4dda8899e7bf3c4756b06b9072d3
gerhus <gebisurgi@gmail.com>
2018-04-07 19:45:36 +0200

    test

--- noname/inputs/pointers.js
+++ noname/inputs/pointers.js
@@ -7,7 +7,7 @@ var Pointers = function (_game) {
 	self.ended = [];
 
 	_game.render.canvas.addEventListener('touchstart', function (event) {
-
+		game.debugger.print(2, event.touches.length);
 		event.preventDefault();
 		for (var i = 0; i < event.changedTouches.length; i++) {
 			var pointer = {
